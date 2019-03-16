06f551dda83f41463ea68f870c32ece57bf67c9d
liqidi <940164665@qq.com>
2019-03-16 11:32:48 +0800

    liqidi

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,53 +3,39 @@
   <el-container class="home">
     <el-header>
       <!-- <el-menu
-        :default-active="activeIndex"
-        mode="horizontal"
-        @select="handleSelect"
-        background-color="transparent"
-        text-color="#fff"
-        active-text-color="#ccc"
-      >
-        <el-menu-item :index="item.name" v-for=" (item,index) in list" :key="index">{{item.label}}</el-menu-item>
+          :default-active="activeIndex"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="transparent"
+          text-color="#b3bdb3"
+          active-text-color="#e2e2da"
+        >
+          <el-menu-item :index="item.name" v-for=" (item,index) in list" :key="index">{{item.label}}</el-menu-item>
       </el-menu>-->
-    </el-header>
-    <el-main>
-      <div class="box" ref="box">
-        <ul class="ul" ref="ul">
-          <li class="li" style="width:60px" ref="li">1</li>
-          <li class="li" style="width:80px" ref="li">2</li>
-          <li class="li" style="width:70px" ref="li">3</li>
-          <li class="li" style="width:66px" ref="li">4</li>
-          <li class="li" style="width:40px" ref="li">5</li>
-          <li class="li" style="width:80px" ref="li">6</li>
-          <li class="li" style="width:24px" ref="li">7</li>
-          <li class="li" style="width:78px" ref="li">8</li>
-          <li class="li" style="width:53px" ref="li">9</li>
-          <li class="li" style="width:87px" ref="li">10</li>
-          <li class="li" style="width:53px" ref="li">11</li>
-          <li class="li" style="width:45px" ref="li">12</li>
-          <li class="li" style="width:65px" ref="li">13</li>
-          <li class="li" style="width:47px" ref="li">14</li>
-          <li class="li" style="width:54px" ref="li">15</li>
-          <li class="li" style="width:52px" ref="li">16</li>
-          <li class="li" style="width:77px" ref="li">17</li>
-          <li class="li" style="width:89px" ref="li">18</li>
-          <li class="li" style="width:45px" ref="li">19</li>
-          <li class="li" style="width:75px" ref="li">20</li>
-          <li class="li" style="width:89px" ref="li">21</li>
+      <div class="header">
+        <ul>
+          <li v-for="(item,index) in nav" :key="index">{{item.name}}</li>
         </ul>
       </div>
-      <el-button type @click="zuo">we</el-button>
-      <el-button type @click="you">we</el-button>
-    </el-main>
+    </el-header>
+    <el-main></el-main>
   </el-container>
 </template>
 <script>
 export default {
   data() {
     return {
-      // activeIndex: 1,
-      // list: [{ name: 1, label: "首页" }, { name: 2, label: "Dom" }]
+      activeIndex: 1,
+      nav: [
+        {
+          name: "首页",
+          index: "/"
+        },
+        {
+          name: "Dom",
+          index: "/Dom"
+        }
+      ],
       liWidth: 0
     };
   },
@@ -75,33 +61,33 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .home {
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
+  background: url("../style/images/preview.jpg");
 }
-.box {
-  width: 80%;
-  margin: 0 auto;
-  background: #ccc;
-  height: 50px;
-  overflow: hidden;
-  position: relative;
+.header {
+  width: 100%;
+  height: 40px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  ul {
+    height: 100%;
+    li {
+      line-height: 40px;
+      display: inline-block;
+      color: #fff;
+      padding: 0 12px;
+      cursor: pointer; //鼠标手势-手指
+    }
+  }
 }
-.ul {
-  height: 50px;
-  white-space: nowrap;
-  width: auto;
-  display: inline-block;
-  position: absolute;
-}
-.li {
-  height: 100%;
-  margin: 0 12px;
-  background: red;
-  display: inline-block;
+</style>
+<style >
+.el-menu.el-menu--horizontal {
+  border-bottom: none;
 }
 </style>
